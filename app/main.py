3ffbcb61988c0b05e5b22ad7dc28eaaa4d827e13
rysterman34 <ryanpapetti@gmail.com>
2022-01-10 15:31:56 -0700

    debugging changes

--- app/main.py
+++ app/main.py
@@ -26,7 +26,7 @@ random.seed(420)
 
 
 #Custom DEBUG mode to make it simpler to run locally or on server. If True, app runs on local port. Use False only for production
-DEBUG_MODE = False
+DEBUG_MODE = True
 
 if DEBUG_MODE:
     # Server-side Parameters
