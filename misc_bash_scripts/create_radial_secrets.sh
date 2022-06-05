73a534aea99009ae0c30b8ac488ec6151a608ed1
rysterman34 <ryanpapetti@gmail.com>
2022-06-05 12:53:38 -0700

    moving files

--- misc_bash_scripts/create_radial_secrets.sh
+++
@@ -1,4 +0,0 @@
-
-aws secretsmanager create-secret \
-    --name radialspotifyauthcreds \
-    --secret-string file://../credentials/radial_spotify_auth.json
--- misc_bash_scripts/instance_connect.sh
+++
@@ -1,1 +0,0 @@
-ssh -i "radial-web-app-keypair.pem" ubuntu@ec2-54-202-137-205.us-west-2.compute.amazonaws.com
